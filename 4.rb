e6966ce3acca522ab0b7ad81b0d0f5f03c5cf902
ChaYoung <yousbe@gmail.com>
2013-08-13 15:26:17 +0900

    Problem 4

---
+++ 4.rb
@@ -0,0 +1,12 @@
+require 'prime'
+
+(100..999).to_a.reverse.each do |num|
+	palin = "#{num}#{num.to_s.reverse}".to_i
+	div = (100..999).lazy.to_a.reverse.select {|v| palin % v == 0 }.first
+	if div and 100 <= palin / div and palin / div < 1000
+		puts palin
+		break
+	end
+end
+
+# 906609
